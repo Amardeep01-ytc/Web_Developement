cbcd906807a6cba89b07803fb013af1e2235a67a
Amardeep Pawar <67967820+Amardeep01-ytc@users.noreply.github.com>
2023-12-29 17:44:46 +0530

    Add files via upload

---
+++ Web_Developement/Front-End Developement/13-JS Intro/script.js
@@ -0,0 +1,8 @@
+alert("hello duniya...  ");
+
+console.log("This is printing stmt in js");
+// anoter way to execute js is a runtime like node.js which can beinstalled and use to run js code.
+// JavsScript can be executed right inside one's browser.
+//  you can open the JavaScript console and start writing js there it will also possible
+
+//we have another way to execute js is by interacting it inside <script> tag of HTML element
