a29ce1c3fd9ebdfd24ce9ef55f616d897642f89e
Amardeep Pawar <67967820+Amardeep01-ytc@users.noreply.github.com>
2024-02-10 08:52:23 +0530

    Script Added

---
+++ Web_Developement/Front-End Developement/15-JS Loops/script.js
@@ -0,0 +1,32 @@
+console.log('Loops code')
+
+
+//Types of loops - 
+// for loops : loop a block of code no of times
+// for in loop : loops throough the key of object 
+// for of loop : loop through the values of object 
+// while loop : loops a block based on specific conditions 
+// do while loop : while loop varient which runs atlest once
+let a = 1 
+for (let i = 0; i < 10; i++) {
+    console.log(a + i);
+    
+}
+
+let obj = {
+    name :  'abc',
+    compeny : 'xyz',
+    salary : '13 lpa'
+}
+for (const key in obj) {
+     {
+        const element = obj[key];
+      console.log(key ,element)  
+    }
+}
+
+var c = [12,14,16,18,20];
+var d = c;
+
+ d.pop();     //     to delete last element in array 
+console.log(d);
