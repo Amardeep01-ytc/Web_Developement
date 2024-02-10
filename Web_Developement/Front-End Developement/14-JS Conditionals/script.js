f3bf2f06e434cc5c43e0ec21fab4fb0b155b6ce0
Amardeep Pawar <67967820+Amardeep01-ytc@users.noreply.github.com>
2024-02-10 08:49:35 +0530

    new commit

---
+++ Web_Developement/Front-End Developement/14-JS Conditionals/script.js
@@ -0,0 +1,22 @@
+console.log("These are conditionals ")
+
+let age= 18
+if(age > 18){
+    console.log('You can Drive');
+}
+else{
+    console.log('You cannot drive');
+}
+
+
+
+
+// if(condition){
+//     console.log(printing stmt);
+// }
+// elseif(another condition){
+//     console.log(printing stmt)
+// }
+// else{
+//     console.log(final printing stmt);
+// }
