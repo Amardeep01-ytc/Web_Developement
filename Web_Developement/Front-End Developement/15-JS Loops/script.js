92e9c77180d6cf900c6287824e44ec9403eb1f37
Amardeep Pawar <67967820+Amardeep01-ytc@users.noreply.github.com>
2024-02-11 23:11:22 +0530

    Update script.js

--- Web_Developement/Front-End Developement/15-JS Loops/script.js
+++ Web_Developement/Front-End Developement/15-JS Loops/script.js
@@ -3,10 +3,10 @@ console.log('Loops code')
 
 //Types of loops - 
 // for loops : loop a block of code no of times
-// for in loop : loops throough the key of object 
-// for of loop : loop through the values of object 
+// for in loop : loops throough the key of object   not uses 
+// for of loop : loop through the values of object   not uses 
 // while loop : loops a block based on specific conditions 
-// do while loop : while loop varient which runs atlest once
+// do while loop : while loop varient which runs atlest once    not uses 
 let a = 1 
 for (let i = 0; i < 10; i++) {
     console.log(a + i);
