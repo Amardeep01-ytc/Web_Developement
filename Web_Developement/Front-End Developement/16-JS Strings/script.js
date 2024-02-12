e597b249632d8caf913680b883a22810f428dc84
Amardeep Pawar <67967820+Amardeep01-ytc@users.noreply.github.com>
2024-02-12 16:12:19 +0530

    New Commit

---
+++ Web_Developement/Front-End Developement/16-JS Strings/script.js
@@ -0,0 +1,24 @@
+console.log("This is String code")
+let a = "JavaScript"
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+console.log(a[6])
+console.log(a[7])
+console.log(a[8])
+console.log(a[9])
+
+
+// console.log(a[10]) //if we add more values than our string it will shows 'undefined'
+console.log(a.length) //it shows the length of array
+console.log(a.toUpperCase())  //makes string in UpperCase
+console.log(a.toLowerCase())  //makes string in LowerCase
+ 
+console.log(a.slice((0,4))) //takes how many characters in string we want
+console.log(a.replace("Java", "Type")) //replaces the character
+console.log(a.concat(" Tutorial")) //can add into strings 
+
+
